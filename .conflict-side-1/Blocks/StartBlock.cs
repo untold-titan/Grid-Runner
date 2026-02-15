a8c15886632a25f371f78accc1a3fe5747fd148d
untold-titan <untoldtitan3k@gmail.com>
2026-02-14 23:39:01 -0700

    Execute Blocks

---
+++ .conflict-side-1/Blocks/StartBlock.cs
@@ -0,0 +1,14 @@
+﻿namespace GridRunner.Interfaces
+{
+    public record class StartBlock : Block
+    {
+        public StartBlock()
+        {
+            isFixed = true;
+        }
+        public override void RunBlock(string state)
+        {
+            Console.WriteLine("Ran Start Block ID: " + Id);
+        }
+    }
+}
---
+++ .conflict-side-1/Interfaces/Block.cs
@@ -0,0 +1,14 @@
+﻿namespace GridRunner.Interfaces
+{
+    public abstract record class Block
+    {
+        public int Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public bool Locked { get; set; }
+        public bool isFixed { get; set; }
+        public string Description { get; set; }
+        public int? ParentId { get; set; }
+        public abstract void RunBlock(string state); // State is placeholder until an actual state is created
+    }
+}
